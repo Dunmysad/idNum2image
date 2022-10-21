35d64016aee86e2dafad953b210065edd03c883d
Dunmysad <1401613533@qq.com>
2022-10-21 20:42:07 +0800

    update

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import idNum2image
+import html2img
 import pandas as pd
+from datetime import datetime
 
 idNums = pd.read_excel(io=r'idNums.xlsx', usecols='A')
 
-for i in idNums.to_dict()['身份证号']:
-    idNum2image.idNum2image(idNums.to_dict()['身份证号'][i])
+def main(idNums, dateTime):
+    for i in idNums.to_dict()['身份证号']:
+        res = idNum2image.idNum2image(idNums.to_dict()['身份证号'][i])
+        if datetime.strptime(dateTime, "%Y-%m-%d") == datetime.strptime(res['samplingTime'].split(' ')[0], "%Y-%m-%d"):
+            html2img.html2image(res)
+            print(f"{res['name']} {dateTime} √")
+        else:
+            print(f"{res['name']} {dateTime} x")
+
+
+if __name__ == '__main__':
+    dateTime = input('请输入日期: xxxx-xx-xx\n')
+    main(idNums, dateTime)
