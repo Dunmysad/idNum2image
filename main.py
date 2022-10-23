4775f17c1781846f3c435e0c7fbeb25b5f55a88e
Dunmysad <1401613533@qq.com>
2022-10-23 20:28:32 +0800

    debug

--- main.py
+++ main.py
@@ -10,30 +10,30 @@ def main(idNums, dateTime):
         pass
     else:
         dateTime = datetime.strftime(datetime.today(), '%Y-%m-%d')
-    
     for i in idNums.to_dict()['身份证号']:
         idNum = idNums.to_dict()['身份证号'][i]
         jsondata_all = idNum2image.idNum2image(idNum)
         # print(type(jsondata_all['data']), jsondata_all['data'])
         if jsondata_all['data']:
             pass
-        else:   
+        else:    
             print(idNum, '未查询到数据')
         for i in range(len(jsondata_all['data'])):
             if jsondata_all['data'][i]['samplingTime'].split(' ')[0] == dateTime:
                 res = jsondata_all['data'][i]
                 html2img.html2image(res)
                 print(jsondata_all['data'][i]['name'], jsondata_all['data'][i]['samplingTime'].split(' ')[0], '√')
+                break
             else:
                 try:
                     print(jsondata_all['data'][0]['name'], dateTime, 'x')
                 except Exception as e:
-                    print('xxx')
                     print(e)
-                break
+                finally:
+                    break
 
 
-                
+    
 if __name__ == '__main__':
     dateTime = input('请输入日期: (格式: xxxx-xx-xx 回车为当天日期)\n')
     main(idNums, dateTime)
